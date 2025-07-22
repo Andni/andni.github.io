7098e5f950b00beca931233d70f114213b6e8b0f
Andreas Niklasson <andreas.cj.niklasson@gmail.com>
2025-07-22 18:38:22 +0200

    creates sdf texture for 'a' and renderes it

--- libs/hello-rust-wasm/src/lib.rs
+++ libs/hello-rust-wasm/src/lib.rs
@@ -1,10 +1,25 @@
-use std::fs;
 use easy_signed_distance_field as sdf;
-use sdf::SdfRaster;
+use wasm_bindgen::prelude::*;
 
-pub fn generate() -> SdfRaster {
-    let font_data:Vec<u8> = fs::read("./test_fixtures/Questrial-Regular.ttf").expect("Failed to read font file");
-    let font = sdf::Font::from_bytes(font_data.as_slice(), Default::default()).expect("Failed to parse font file");
+
+#[wasm_bindgen]
+pub struct WasmSdfRaster {
+    pub width: u32,
+    pub height: u32,
+    data: Vec<f32>,
+}
+
+#[wasm_bindgen]
+impl WasmSdfRaster {
+    #[wasm_bindgen(getter)]
+    pub fn data(&self) -> Vec<f32> {
+        self.data.clone()
+    }
+}
+
+#[wasm_bindgen]
+pub fn generate_sdf(font_data: &[u8]) -> WasmSdfRaster {
+    let font: sdf::Font = sdf::Font::from_bytes(font_data, Default::default()).expect("Failed to parse font file");
 
     let px:f32 = 64.0;
     let padding:i32 = 2;
@@ -12,14 +27,19 @@ pub fn generate() -> SdfRaster {
     let (_a_metrics, a_glyph_sdf) = font.sdf_generate(px, padding, spread, 'a').unwrap();
 
 
-    return a_glyph_sdf;
-    #[cfg(feature="export")]
-    sdf::sdf_to_file("test_outputs/font_a.png", &a_glyph_sdf).unwrap();
+    return WasmSdfRaster {
+        width: a_glyph_sdf.width,
+        height: a_glyph_sdf.height,
+        data: a_glyph_sdf.buffer,
+    };
 
-    #[cfg(feature="render")]
-    #[cfg(feature="export")]
-    {
-        let render_scale: f32 = 1.0;
-        sdf::sdf_render_to_file("test_outputs/font_a_render.png", render_scale, 0.5, 0.02, &a_glyph_sdf).unwrap();
-    }
+    // #[cfg(feature="export")]
+    // sdf::sdf_to_file("test_outputs/font_a.png", &a_glyph_sdf).unwrap();
+
+    // #[cfg(feature="render")]
+    // #[cfg(feature="export")]
+    // {
+    //     let render_scale: f32 = 1.0;
+    //     sdf::sdf_render_to_file("test_outputs/font_a_render.png", render_scale, 0.5, 0.02, &a_glyph_sdf).unwrap();
+    // }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { Application, BufferImageSource, Sprite, Texture } from "pixi.js";
 
-// import * as wasm from '../libs/hello-rust-wasm/pkg';
+import * as wasm from '../libs/hello-rust-wasm/pkg';
 
 
 (async () => {
@@ -10,27 +10,37 @@ import { Application, BufferImageSource, Sprite, Texture } from "pixi.js";
   // Initialize the application
   await app.init({ background: "#1099bb", resizeTo: window });
 
+
+  const response = await fetch('/BrushStrokes-q6B5.ttf');
+  const fontData = await response.arrayBuffer();
+
+
+
   // Append the application canvas to the document body
   document.getElementById("pixi-container")!.appendChild(app.canvas);
-  const width = 256;
-  const height = 256;
-  const length = width * height * 4;
-  const pixels = Uint8Array.from({ length }, () => Math.floor(Math.random() * 255));
+  // const width = 256;
+  // const height = 256;
+  const sdf = wasm.generate_sdf(new Uint8Array(fontData));
+
+  console.log(sdf.width, sdf.height, sdf.data);
+  // const length = bufferPixels.height * bufferPixels.width;
+  // const pixels = Uint8Array.from({ length }, () => Math.floor(Math.random() * 255));
+  // const pixels = Float32Array.from(sdf.data.map(val => val));
 
   // Create BufferResource for audio texture
-  const pixelResource = new BufferImageSource({ resource: pixels, width, height });
+  const pixelResource = new BufferImageSource({ resource: sdf.data, width: sdf.width, height: sdf.height, format: "r32float" });
   const audioTexture = new Texture({ source: pixelResource });
 
   const sprite = app.stage.addChild(new Sprite(audioTexture));
+  sprite.scale = 10;
 
 
-  app.ticker.add(() => {
-    pixels.forEach((_, i) => {
-      pixels[i] = Math.floor(Math.random() * 255);
-    });
-    pixelResource.update();
-  });
-
+  // app.ticker.add(() => {
+  //   pixels.forEach((_, i) => {
+  //     pixels[i] = Math.floor(Math.random() * 255);
+  //   });
+  //   pixelResource.update();
+  // }); 
   // 6. Add the sprite to the stage
   app.stage.addChild(sprite);
 
